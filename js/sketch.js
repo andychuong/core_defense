6bc7bfaf5ed7d4e54f020572fb1f38da20ca3d78
Andy Chuong <andy.chuong@colorado.edu>
2018-09-14 12:24:57 -0600

    tests

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,14 @@
+let score = 0
+let gameStage = 0
+let health = 100
+
+
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   // put drawing code here
+    background(102);
 }
