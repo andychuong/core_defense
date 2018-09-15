14322e267219469f710bf9cb600edf3956627730
Andy Chuong <andy.chuong@colorado.edu>
2018-09-15 16:53:52 -0600

    update read me

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# core_defense
+# Core Defense
+
+Simple browser based defense game
+
+## Game Basics
+
+The goal of this game is to protect the core. There are four shields floating around the core that can be rotated. Each shield is 90 degrees from the other.
+
+## Winning and Losing
+
+* Getting the core hit will result in your health depleting. If you reach 0 health, you will lose
+* Points can be gained by using your shield to block projectiles.
+* When all projectiles are blocked, the round is over.
+
+## Controls
+
+Work in progress....
+
+### Technology
+* HTML, CSS, JavaScript
+* p5.js, p5.play.js
--- js/sketch.js
+++ js/sketch.js
@@ -8,8 +8,6 @@ let deg2 = Math.PI/2
 let deg3 = -Math.PI/2
 let deg4 = 2*Math.PI
 
-
-
 function setup() {
   // put setup code here
   createCanvas(windowWidth -5, windowHeight-5);
@@ -22,52 +20,28 @@ function draw() {
     background(255);
     fill(10)
     rect(100,100, windowWidth - 200, windowHeight - 200);
+
+    // Draw player 
     fill(255,0,0)
     ellipseMode(CENTER)
     ellipse(midX, midY, 25, 25)
-    //45
+
     fill(0,255,0)
-    // 0 degrees
-    // ellipse(midX + 50, midY, 20, 20)
-    // 45 degrees
-    // ellipse(midX + (50*sqrt(2)/2), midY-(50*sqrt(2)/2), 20, 20)
-    // 90 degrees
-    // ellipse(midX, midY - 50, 20, 20)
-    // 135
-    // ellipse(midX - (50*sqrt(2)/2), midY-(50*sqrt(2)/2), 20, 20)
-    // 180
-    // ellipse(midX - 50, midY, 20, 20)
-    // 225
-    // ellipse(midX - (50*sqrt(2)/2), midY+(50*sqrt(2)/2), 20, 20)
-    // 270
-    // ellipse(midX, midY + 50, 20, 20)
-    // 315
-    // ellipse(midX + (50*sqrt(2)/2), midY+(50*sqrt(2)/2), 20, 20)
 
-    // if (deg === true) {
-    //   ellipse(midX + (50*sqrt(2)/2), midY-(50*sqrt(2)/2), 20, 20)
-    //   ellipse(midX - (50*sqrt(2)/2), midY-(50*sqrt(2)/2), 20, 20)
-    //   ellipse(midX - (50*sqrt(2)/2), midY+(50*sqrt(2)/2), 20, 20)
-    //   ellipse(midX + (50*sqrt(2)/2), midY+(50*sqrt(2)/2), 20, 20)
-    // }
-    // if (deg === false) {
-    //   ellipse(midX + 50, midY, 20, 20)
-    //   ellipse(midX, midY - 50, 20, 20)
-    //   ellipse(midX - 50, midY, 20, 20)
-    //   ellipse(midX, midY + 50, 20, 20)
-    // }
-
-    // ellipse(midX + (50*cos(deg)), midY + (50*sin(deg)), 20, 20)
+    // Adjust origin
     translate(width/2,height/2);
+    // Draw shields
     ellipse(50 * cos(deg), 50 * sin(deg), 20, 20)
     ellipse(50 * cos(deg2), 50 * sin(deg2), 20, 20)
     ellipse(50 * cos(deg3), 50 * sin(deg3), 20, 20)
     ellipse(50 * cos(deg4), 50 * sin(deg4), 20, 20)
 
 }
+
 // function mousePressed() {
 //   deg -= .5
 // }
+
 function keyPressed() {
     if(key === "a"){
       deg -= .25
