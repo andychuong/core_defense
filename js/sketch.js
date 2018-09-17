75c84d21f7c3557bd319d99a141e0ac9b7066199
Andy Chuong <andy.chuong@colorado.edu>
2018-09-17 15:14:13 -0600

    image test

--- js/sketch.js
+++ js/sketch.js
@@ -12,8 +12,8 @@ let health = 100
 let coreX
 let coreY
 // Speed Controls
-let rotateSpd = .05
-let playerSpd = 1.5
+let rotateSpd = .06
+let playerSpd = 2
 // Set up core and shields
 let coreSize = 40
 let myCore
@@ -49,7 +49,7 @@ function setup() {
   coreX = width / 2
   coreY = height / 2
   // Difficulty and spawns (spawns = 5 * diff)
-  diff = 5
+  diff = 3
   // Collision groups
   shields = new Group()
   projectiles = new Group()
@@ -67,16 +67,16 @@ function draw() {
   // Reset Background before drawing
   background(10);
   // Update Core's x, y
-  updateCore()
+  moveCore()
   // Update Shields' rotations
-  updateShields()
+  rotateShields()
   // Bounds check
   wallBounce()
   // Collision
   projectiles.overlap(shields, hitShield);
   projectiles.overlap(myCore, hitCore)
   // Draw all Sprites
-    drawSprites();
+  drawSprites();
 }
 
 function createWalls() {
@@ -92,55 +92,78 @@ function createWalls() {
   wallRight = createSprite(width + wallThickness / 2, height / 2, wallThickness, height);
   wallRight.immovable = true;
 }
-function wallBounce(){
+
+function wallBounce() {
   projectiles.bounce(wallTop);
   projectiles.bounce(wallBottom);
   projectiles.bounce(wallLeft);
   projectiles.bounce(wallRight);
 }
-function createCore(){
-  myCore = createSprite(coreX,coreY, coreSize, coreSize)
-  myCore.setCollider('circle', 0, 0, coreSize + 20)
+
+function createCore() {
+  myCore = createSprite(coreX, coreY, coreSize, coreSize)
+  myCore.setCollider('circle', 0, 0, coreSize + 5)
+  myCore.shapeColor = "#00ddff"
 }
-function updateCore() {
+
+function moveCore() {
   if (keyDown('w')) {
-    if(coreY > armLength + shieldSize/2){
+    if (coreY > armLength + shieldSize / 2) {
       coreY -= playerSpd
     }
   }
   if (keyDown('s')) {
-    if(coreY < height - armLength - shieldSize/2){
+    if (coreY < height - armLength - shieldSize / 2) {
       coreY += playerSpd
     }
   }
   if (keyDown('a')) {
-    if(coreX > armLength + shieldSize/2 ){
+    if (coreX > armLength + shieldSize / 2) {
       coreX -= playerSpd * 1.2
     }
   }
   if (keyDown('d')) {
-    if(coreX < width - armLength - shieldSize/2){
+    if (coreX < width - armLength - shieldSize / 2) {
       coreX += playerSpd * 1.2
     }
   }
-  myCore.position.x = constrain(coreX,0+armLength, width - armLength)
-  myCore.position.y = constrain(coreY,0-armLength, height - armLength)
+  myCore.position.x = constrain(coreX, 0 + armLength, width - armLength)
+  myCore.position.y = constrain(coreY, 0 - armLength, height - armLength)
 }
+
 function createShields() {
-  shield0 = createSprite(coreX + armLength * cos(deg0), coreY + armLength * sin(deg0), shieldSize, shieldSize)
+  let img = loadImage('img/shield1.png');
+
+  shield0 = createSprite(coreX + armLength * cos(deg0), coreY + armLength * sin(deg0))
   shields.add(shield0)
-  shield0.setCollider('circle', 0, 0, shieldSize -5)
-  shield1 = createSprite(coreX + armLength * cos(deg1), coreY + armLength * sin(deg1), shieldSize, shieldSize)
+  shield0.setCollider('circle', 0, 0, 28)
+  shield0.shapeColor = "#00ff00"
+  shield0.addImage(img);
+  shield0.scale = 0.4;
+
+  shield1 = createSprite(coreX + armLength * cos(deg1), coreY + armLength * sin(deg1))
   shields.add(shield1)
-  shield1.setCollider('circle', 0, 0, shieldSize -5)
-  shield2 = createSprite(coreX + armLength * cos(deg2), coreY + armLength * sin(deg2), shieldSize, shieldSize)
+  shield1.setCollider('circle', 0, 0, 28)
+  shield1.shapeColor = "#00ff00"
+  shield1.addImage(img);
+  shield1.scale = 0.4;
+
+  shield2 = createSprite(coreX + armLength * cos(deg2), coreY + armLength * sin(deg2))
   shields.add(shield2)
-  shield2.setCollider('circle', 0, 0, shieldSize -5)
-  shield3 = createSprite(coreX + armLength * cos(deg3), coreY + armLength * sin(deg3), shieldSize, shieldSize)
+  shield2.setCollider('circle', 0, 0, 28)
+  shield2.shapeColor = "#00ff00"
+  shield2.addImage(img);
+  shield2.scale = 0.4;
+
+  shield3 = createSprite(coreX + armLength * cos(deg3), coreY + armLength * sin(deg3))
   shields.add(shield3)
-  shield3.setCollider('circle', 0, 0, shieldSize -5)
+  shield3.setCollider('circle', 0, 0, 28)
+  shield3.shapeColor = "#00ff00"
+  shield3.addImage(img);
+  shield3.scale = 0.4;
 }
-function updateShields() {
+
+function rotateShields() {
   if (keyDown(LEFT_ARROW)) {
     deg0 -= rotateSpd
     deg1 -= rotateSpd
@@ -165,8 +188,9 @@ function updateShields() {
   shield3.position.x = (coreX + armLength * cos(deg3))
   shield3.position.y = (coreY + armLength * sin(deg3))
 }
-function createProjectiles(diff){
-  for (let i = 0; i < (diff * 10); i++) {
+
+function createProjectiles(diff) {
+  for (let i = 0; i < (diff * 8); i++) {
     // set random direction
     let px = width * random()
     let py = height * random()
@@ -174,36 +198,48 @@ function createProjectiles(diff){
     createProjectile(px, py)
   }
 }
+
 function createProjectile(x, y) {
   let a = createSprite(x, y, 50, 50);
   // var img = loadImage('assets/asteroid'+floor(random(0, 3))+'.png');
   // a.addImage(img);
-  a.setSpeed(7, random(360))
+  // a.setSpeed(4.5, random(360))
+  a.setSpeed(2, random(360))
   a.rotationSpeed = 0.5
   //a.debug = true;
   a.scale = 0.2
+  a.shapeColor = "#ff2222"
   a.setCollider('circle', 0, 0, 10)
   projectiles.add(a)
   return a;
 }
-function hitShield(projectile,shield) {
+
+function hitShield(projectile, shield) {
   // health -= 10;
   // if (health === 0) {
   //   gg()
   // }
-  console.log('hit')
+  // console.log('hit')
   projectile.remove();
+  score += diff
+  console.log(`score: ${score}`)
 }
-function hitCore(projectile,myCore){
-  console.log('corehit')
+
+function hitCore(projectile, myCore) {
+  // console.log('corehit')
   projectile.remove()
+  health -= 5
+  console.log(`hp: ${health} `)
 }
+
 function getHp() {
   health += 10
 }
+
 function gg() {
   gameStage = 2
 }
+
 function windowResized() {
   let canvasDiv = document.getElementById('canvasDiv')
   let width = canvasDiv.offsetWidth
